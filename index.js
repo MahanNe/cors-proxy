227e7e887b2477e5097074ba95f2211361c969df
MahanNe <159655797+MahanNe@users.noreply.github.com>
2024-06-30 09:11:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const corsAnywhere = require('cors-anywhere');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 
 // Serve static files (your HTML/CSS/JS)
 app.use(express.static(path.join(__dirname, 'public')));
