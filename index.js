6824d4bdf0a7c4ca14af15e3c219aa4b507987e5
MahanNe <159655797+MahanNe@users.noreply.github.com>
2024-07-04 09:17:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,105 @@
-const express = require('express');
-const corsAnywhere = require('cors-anywhere');
-const path = require('path');
+import express, { json } from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
+import instagramGetUrl from "instagram-url-direct";
 
 const app = express();
-const port = process.env.PORT || 5500;
+app.use(cors());
+app.use(json());
 
-// Serve static files (your HTML/CSS/JS)
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(4000, () => {
+  console.log("Server Works !!! At port 4000");
+});
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello World",
+  });
+});
+app.get("/pinterest", async (req, res) => {
+  var url = req.query.url;
+  try {
+    if (url.match("pin.it")) url = await expandURL(url);
+    
+    const { hostname, pathname } = new URL(url);
+    const path = pathname.replace("/sent/", "");
+    const finalUrl = `https://${hostname}${path}`;
+    const response = await fetch(finalUrl);
+    if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`);
+    }
+    const body = await response.text();
+    let outUrl;
+    let type = "video";
+    try {
+        const video = new JSDOM(body).window.document.getElementsByTagName(
+            "video"
+        )[0].src;
+        outUrl = video.replace("/hls/", "/720p/").replace(".m3u8", ".mp4");
+    } catch (_) {
+        outUrl = new JSDOM(body).window.document.getElementsByTagName(
+            "img"
+        )[0].src;
+        type = "image";
+    }
 
-// Set up the CORS Anywhere proxy
-const corsProxy = corsAnywhere.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+    const title = new JSDOM(body).window.document.querySelector('div[data-test-id="pinTitle"] h1').innerHTML;
+    var desc;
+    try {
+        // Description may not be available
+        desc = new JSDOM(body).window.document.querySelector('div[data-test-id="truncated-description"] div div span').innerHTML;
+    } catch (_) {}
+
+    console.log(outUrl);
+
+    res.status(200).send({
+        url: outUrl,
+        title: url.match("pin.it") ? "Pinterest shorten url" : "Pinterest full url",
+        type: type,
+        titleURL: title,
+        decsURL: desc
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+app.get("/instagram", async (req, res) => {
+  const url = req.query.url;
+  try {
+    if (url.match("instagram.com")) {
+      const rsd = await instagramGetUrl(url);
+
+      const uri = rsd.url_list[0];
+
+      res.status(200).send({
+        url: uri,
+        title: "Instagram shorten url",
+      });
+    } else {
+      res.status(200).send({
+        error: "Not a valid url",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
 });
 
-app.use('/proxy', (req, res) => {
-    req.url = req.url.replace('/proxy/', '/');
-    corsProxy.emit('request', req, res);
-});
-
-app.listen(port, () => {
-    console.log(`CORS Proxy server running on port ${port}`);
-});
+async function expandURL(shortenURL) {
+  const uri = new URL(shortenURL);
+  const path = uri.pathname;
+  const finalUrl = `https://api.pinterest.com/url_shortener${path}/redirect/`;
+  try {
+    let response = await fetch(finalUrl, {
+      method: "HEAD",
+      redirect: "manual",
+    });
+    let location = response.headers.get("location");
+    return location;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
